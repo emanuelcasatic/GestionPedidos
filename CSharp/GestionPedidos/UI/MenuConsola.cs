e19a67aa1535a7bae5b9b2152ba11dd94830390f
SrLinh <mariostevjimenez@gmail.com>
2026-07-22 11:22:02 -0600

    Menu terminado

--- CSharp/GestionPedidos/UI/MenuConsola.cs
+++ CSharp/GestionPedidos/UI/MenuConsola.cs
@@ -27,6 +27,30 @@ namespace GestionPedidos.UI
                 Console.Write("Seleccione una opcion: ");
                 opcion = Convert.ToInt32(Console.ReadLine());
 
+                switch (opcion)
+                {
+                    case 1:
+                    break;
+                    case 2:
+                    break;
+                    case 3:
+                    break;
+                    case 4:
+                    break;
+                    case 5:
+                    break;
+                    case 6:
+                    break;
+                    case 7:
+                    break;
+                    case 8:
+                    break;
+                    case 9:
+                    break;
+                    case 10:
+                    break;
+                    
+                }
             }
         }
     }
