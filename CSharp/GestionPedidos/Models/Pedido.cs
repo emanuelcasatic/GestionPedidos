d84532f4a9313ba7bb4f08f72ea150cff80a9b80
emanuelcasatic <emanuelcasatic@gmail.com>
2026-07-22 10:20:23 -0600

    Primer subida del proyecto: estructura del proyecto

---
+++ CSharp/GestionPedidos/Models/Pedido.cs
@@ -0,0 +1,26 @@
+/*Enfoque: Arquitectura del proyecto, modelo de datos y persistencia JSON.
+Archivos asignados: Models/Pedido.cs, Persistence/GestorJson.cs, Data/RepositorioPedidos.cs
+Tareas principales:
+Crear el modelo Pedido con todos sus atributos (código, cliente, producto, cantidad, precio, entrega, fecha, estado) y propiedades calculadas (Subtotal, Total, CostoEntrega).
+Desarrollar GestorJson.cs usando System.Text.Json para leer/escribir el archivo pedidos.json.
+Diseñar la estructura inicial del repositorio Git, ramas y reglas de colaboración.
+
+
+ Código del pedido.
+ Nombre del cliente.
+ Producto solicitado.
+ Cantidad.
+ Precio unitario.
+ Tipo de entrega.
+ Fecha del pedido.
+ Estado del pedido.
+*/
+
+using System;
+
+namespace GestionPedidos.Models; 
+
+public class Pedido
+{
+    
+} 
---
+++ CSharp/GestionPedidos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GestionPedidos
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("¡Hola, Mundo!");
+        }
+    }
+}
